3353532f2df9f72f31c2155933e223601d03004a
Abhishek Rai <raiabhi2176@gmail.com>
2024-07-29 15:05:56 +0530

    Portfolio

---
+++ src/components/Icons.jsx
@@ -0,0 +1,36 @@
+import { faInstagram, faTwitter, faGithub, faLinkedin, faCodepen } from '@fortawesome/free-brands-svg-icons';
+
+export const Icons = [
+    {
+        icon: faLinkedin,
+        name: "Linkedin",
+        BGcolor: "#0a66c2",
+        link: "https://www.linkedin.com/in/abhishek-rai-b7b907204/"
+    },
+    {
+        icon: faInstagram,
+        name: "Instagram",
+        BGcolor: "#e4405f",
+        link: "https://www.instagram.com/rai_abhi_1024/"
+    },
+    // {
+    //     icon: faTwitter,
+    //     name: "Twitter",
+    //     BGcolor: "#1da1f2",
+    //     link: "https://twitter.com/"
+    // },
+    // {
+    //     icon: faCodepen,
+    //     name: "CodeStudio",
+    //     BGcolor: "#000",
+    //     link: ""
+    // },
+    {
+        icon: faGithub,
+        name: "Github",
+        BGcolor: "#222",
+        link: "https://github.com/raiabhi07",
+    },
+
+];
+
